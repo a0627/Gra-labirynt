f50ee5a35e7c6a173492333afa8d13d33c399dca
a0627 <145047858+a0627@users.noreply.github.com>
2023-10-26 20:02:56 +0200

    implementation

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -10,6 +10,24 @@ public class PortalCamera : MonoBehaviour
 
     void Update()
     {
+        PortalCameraController();
+    }
+
+    private void PortalCameraController()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        //mo¿esz pododawaæ wypisy do konsoli
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(
+            portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(
+            angularDifferenceBetweenPortalRotations, Vector3.up);
+       Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        newCameraDirection = new Vector3(newCameraDirection.x * -1,
+            newCameraDirection.y, newCameraDirection.z * -1);
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
 
     }
 
